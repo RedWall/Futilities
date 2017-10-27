1f905873d4232a7b2c2a56bef64d2862c6abd27b
Jeremy Deckard <Jeremy@teamredwall.com>
2017-10-26 17:50:42 -0700

    Move file and object hashing to new projects

---
+++ Futilities/Futilities.Hash/Hash.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+
+namespace Futilities.Hashing
+{
+    public static class Hash
+    {
+        public static void SetHash<T>(this T obj, Expression<Func<T, string>> selector) where T : IComputeHash
+        {
+            var p = (PropertyInfo)((MemberExpression)selector.Body).Member;
+            p.SetValue(obj, ComputeHash<T>(obj));
+        }
+
+        public static string ComputeHash<T>(this T obj) where T : IComputeHash
+        {
+            using (var ms = new MemoryStream())
+            {
+
+                var formatter = new BinaryFormatter();
+
+                var dict = (obj.GetObjectToCompute() as IDictionary<string, object>).ToDictionary(x => x.Key, x => x.Value);
+
+                formatter.Serialize(ms, dict);
+
+                using (var md = MD5.Create())
+                {
+                    var bytes = md.ComputeHash(ms.ToArray());
+
+                    return BitConverter.ToString(bytes);
+                }
+            }
+        }
+    }
+}
