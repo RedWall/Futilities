4c213efdc0bc949fdc612450756ad5498f1029bb
Jimmie Houts <Jimmie@teamredwall.com>
2021-10-30 11:16:32 -0700

    Added string To SHA1 hash Added unit tests Cleaned up unit test names

--- Futilities/Futilities.Hash/Hash.cs
+++ Futilities/Futilities.Hash/Hash.cs
@@ -26,7 +26,6 @@ namespace Futilities.Hashing
 
             using (var ms = new MemoryStream())
             {
-
                 var formatter = new BinaryFormatter();
 
                 var dict = (obj.GetObjectToCompute() as IDictionary<string, object>).ToDictionary(x => x.Key, x => x.Value);
@@ -66,8 +65,29 @@ namespace Futilities.Hashing
             if (value is null)
                 throw new ArgumentNullException(nameof(value));
 
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+
             using var hash = SHA256.Create();
-            return string.Concat(hash.ComputeHash(Encoding.UTF8.GetBytes(value)).Select(i => i.ToString("x2")));
+
+            return string.Concat(hash.ComputeHash(bytes).Select(i => i.ToString("x2")));
+        }
+
+        /// <summary>
+        /// Hashes a string using <see cref="SHA1"></see>
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <returns>A string hashed using <see cref="SHA1"></see></returns>
+        public static string ToSHA1(this string value)
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            using var hash = SHA1.Create();
+            
+            return string.Concat(hash.ComputeHash(bytes).Select(b => b.ToString("x2")));
         }
     }
 }
